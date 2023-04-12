9204c854824630a600013264921670651ebcdefe
Mubarak Ali <mubi20018@gmail.com>
2023-04-12 14:08:54 +0500

    code cleanup

--- core/network/src/main/java/com/northsoltech/network/datasource/SigningDataSourceImp.kt
+++ core/network/src/main/java/com/northsoltech/network/datasource/SigningDataSourceImp.kt
@@ -1,21 +1,13 @@
 package com.northsoltech.network.datasource
 
 import com.northsoltech.domain.models.ApiResource
-import com.northsoltech.domain.models.getDataOrNull
 import com.northsoltech.domain.models.loging.LoginData
 import com.northsoltech.domain.models.transform
 import com.northsoltech.domain.remote.SigningDataSource
 import com.northsoltech.network.models.signing.LoginResponse
 import com.northsoltech.network.services.ApiServices
-import com.northsoltech.network.utils.getErrorMessage
-import com.northsoltech.network.utils.getNetworkErrorMessage
-import io.ktor.client.call.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.transform
 
 class SigningDataSourceImp constructor(
     private val apiServices: ApiServices
